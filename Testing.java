8cb456c26c3e46ad30cc58e3806e942472a1cb26
Slendrum465 <32966925+Slendrum465@users.noreply.github.com>
2017-11-08 14:00:03 -0500

    Create Testing.java

---
+++ Testing.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Testing {
+
+
+    public static void main(String[] args) {
+
+        Scanner userIn = new Scanner(System.in);
+
+        int num1 = userIn.nextInt();
+
+    }
+}
